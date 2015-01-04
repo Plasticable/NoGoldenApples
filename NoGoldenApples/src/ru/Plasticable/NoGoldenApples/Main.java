010acb57dffcb848dd4c1e37601e4d674882f4ee
Plasticable <plastic@xaker.ru>
2015-01-05 03:52:14 +0400

    Test

---
+++ NoGoldenApples/src/ru/Plasticable/NoGoldenApples/Main.java
@@ -0,0 +1,61 @@
+package ru.Plasticable.NoGoldenApples;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+
+public class Main extends JavaPlugin implements Listener
+{
+	private FileConfiguration config;
+	private String joinNotify, warning, globalNotify;
+	private int potionDuration, potionAmplifier;
+	private boolean globalNotifyEnabeled, joinNotifyEnabled;
+	
+	public void onEnable()
+	{
+		getServer().getPluginManager().registerEvents(this, this);
+		config = getConfig();
+		
+		joinNotifyEnabled = config.getBoolean("joinNotifyEnabled");
+		joinNotify = config.getString("joinNotify");
+		warning = config.getString("warning");
+		globalNotify = config.getString("globalNotify");
+		globalNotifyEnabeled = config.getBoolean("isGlobalNotifyEnabled");
+		potionDuration = config.getInt("potionDuration");
+		potionAmplifier = config.getInt("potionAmplifier");
+				
+	}
+
+	
+	@EventHandler
+	public void onJoin(PlayerJoinEvent e)
+	{
+		if(joinNotifyEnabled)
+			e.getPlayer().sendMessage(ChatColor.YELLOW + joinNotify);
+	}
+	
+	
+	@EventHandler
+	public void onItemConsume(PlayerItemConsumeEvent event)
+	{
+		Player player = event.getPlayer();
+		if(player.getItemInHand().getType() == Material.GOLDEN_APPLE)
+		{
+			player.sendMessage(ChatColor.RED + warning);
+			if(globalNotifyEnabeled)
+				player.getServer().broadcastMessage(ChatColor.YELLOW + globalNotify.replace("{PLAYER}", player.getName()));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.POISON, potionDuration*20, potionAmplifier));
+			event.setCancelled(true);
+		}
+		
+	}
+}
